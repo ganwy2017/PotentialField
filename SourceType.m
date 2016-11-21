27d7e9dc1fbe69cbd1d0e910f58f049d1be4c24a
Arthur <Arth-ur@users.noreply.github.com>
2016-11-21 13:04:19 +0100

    Add UI control

--- SourceType.m
+++ SourceType.m
@@ -1,7 +1,20 @@
 classdef SourceType
     
+    properties
+        K
+    end
+    
+    methods
+        function obj = SourceType(K)
+            if nargin > 0
+                obj.K = K;
+            end
+        end
+    end
+    
+    
     enumeration
-        REPULSIVE, ATTRACTIVE, VOID
+        REPULSIVE(-1), ATTRACTIVE(+1000), VOID(0)
     end    
 end
 
